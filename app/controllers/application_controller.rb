42ccff1d6ac54d6245863d96887fbfdc9ff5e1cb
Scott Lorimor <scott.lorimor28@gmail.com>
2016-12-10 23:13:24 -0700

    fix view bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render :html "HOW THE OTHER HALF LIVE."
+    render html: "HOW THE OTHER HALF LIVE."
   end
 end
